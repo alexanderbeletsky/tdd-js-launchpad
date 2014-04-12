74e7e05a26add723d11ef1364f82369dd0b3f8c9
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-04-12 07:37:34 +0300

    bower and source

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,11 +7,15 @@ module.exports = function(grunt) {
       all: {
         src: ['source/**/*.js'],
         options: {
-          specs: ['test/spec/**/*.js']
+          specs: ['test/spec/**/*.js'],
+          vendor: [
+            'bower_components/jquery/dist/jquery.js'
+          ]
         }
       }
     }
   });
+
   // Laoded tasks
   grunt.loadNpmTasks('grunt-contrib-jasmine');
 
