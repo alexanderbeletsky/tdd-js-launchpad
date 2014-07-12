612cd66dd137a4f60cadc60a6b464ba51d810647
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-12 08:02:20 +0300

    current implementation

--- source/index.js
+++ source/index.js
@@ -1,5 +1,36 @@
 var app = app || {};
 
 $(function () {
+	var widget = app.TemperatureWidget = function ($container) {
+		this.$container = $container;
+		this.init();
+	};
 
+	widget.prototype.init = function (callback) {
+		var me = this;
+
+		$.get('/api/temperature', function (data) {
+			me.data = data;
+
+			callback && callback();
+		});
+	};
+
+	widget.prototype.render = function () {
+		this.$ = $('<div class="temperature-widget">\
+			<div class="temperature">[TEMPERATURE] C</div>\
+			<button class="temperature-fetch">Update</div>\
+		</div>'.replace('[TEMPERATURE]', this.data.temperature));
+
+		this.$.find('.temperature-fetch').on('click', $.proxy(this.updateTemperature, this));
+
+		this.$container.html(this.$);
+	};
+
+	widget.prototype.updateTemperature = function () {
+		var me = this;
+		this.init(function () {
+			me.render();
+		});
+	};
 });
--- test/spec/index.js
+++ test/spec/index.js
@@ -1,5 +1,69 @@
-describe('spec', function () {
-	it('should be failing', function () {
-		expect(1).toEqual(0);
+/* globals jasmine, app */
+describe('Temperature widget spec', function () {
+	var $container, widget;
+
+	beforeEach(function () {
+		jasmine.Ajax.install();
+	});
+
+	afterEach(function () {
+		jasmine.Ajax.uninstall();
+	});
+
+	beforeEach(function () {
+		$container = $('<div class="test-container">aaaaaaa</div>');
+		$container.appendTo('body');
+	});
+
+	afterEach(function () {
+		$container.remove();
+	});
+
+	beforeEach(function () {
+		widget = new app.TemperatureWidget($container);
+	});
+
+	beforeEach(function () {
+		jasmine.Ajax.requests.mostRecent().response({
+			"status": 200,
+			"contentType": 'application/json',
+			"responseText": JSON.stringify({temperature: 27})
+		});
+	});
+
+	it('should fetch the data', function () {
+		expect(jasmine.Ajax.requests.mostRecent().url).toEqual('/api/temperature');
+	});
+
+	describe('when rendered', function () {
+		beforeEach(function () {
+			widget.render();
+		});
+
+		it('should render widget container', function () {
+			expect($container.find('.temperature-widget').length).toEqual(1);
+		});
+
+		it('should render temperature', function () {
+			expect($container.find('.temperature-widget .temperature').text()).toEqual('27 C');
+		});
+
+		describe('and temperatur re-fetched', function () {
+			beforeEach(function () {
+				$container.find('button.temperature-fetch').click();
+			});
+
+			beforeEach(function () {
+				jasmine.Ajax.requests.mostRecent().response({
+					"status": 200,
+					"contentType": 'application/json',
+					"responseText": JSON.stringify({temperature: 22})
+				});
+			});
+
+			it('should update temperature', function () {
+				expect($container.find('.temperature-widget .temperature').text()).toEqual('22 C');
+			});
+		});
 	});
 });
